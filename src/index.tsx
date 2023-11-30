576e62ce223f79d45452523786529d2874ba6403
Vũ Tiến Đức <100019107+ducvu0sdsd@users.noreply.github.com>
2023-12-01 06:08:20 +0700

    upload

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios'
-axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = 'https://qiflix-be.vercel.app';
 
 
 ReactDOM.render(
