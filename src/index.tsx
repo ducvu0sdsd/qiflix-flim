c49c80133f6f637acf8857c4245d7280105881b6
Vũ Tiến Đức <100019107+ducvu0sdsd@users.noreply.github.com>
2023-12-18 23:41:51 +0700

    d

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,8 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://qiflix-be.vercel.app';
-// axios.defaults.baseURL = 'http://localhost:8080';
+// axios.defaults.baseURL = 'https://qiflix-be.vercel.app';
+axios.defaults.baseURL = 'http://localhost:8080';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
